375e12cf322fe8c90a65bf3667ab3456eefcb8af
Vikram7007 <lalevikra32@gmail.com>
2025-12-05 17:56:47 +0530

    added all file

---
+++ project_1/src/App.jsx
@@ -0,0 +1,40 @@
+
+
+
+function App(){
+  return (
+  <>
+  <div id ="parent">
+    <h1 id="child1">like kardo !!!</h1>
+    <p id="child2">vikram lale</p>
+  </div>
+  <div className="chacha"></div>
+  </>
+  )
+} 
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
